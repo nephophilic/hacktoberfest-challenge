41b387047d42ce3d43526c85f7b0fde493817348
Divya2k05 <divyaat2005@gmail.com>
2022-10-23 18:07:49 +0530

    end zeros

---
+++ python missions/End Zeros.py
@@ -0,0 +1,29 @@
+# Try to find out how many zeros a given number has at the end.
+
+def end_zeros(num: int) -> int:
+    #your code here 
+    return None
+    
+print('Example:')
+print(end_zeros(10))
+
+
+
+assert end_zeros(10) == 1
+assert end_zeros(10001001) == 0
+assert end_zeros(1000) == 3
+assert end_zeros(100) == 2
+assert end_zeros(20010) == 1
+assert end_zeros(100001000) == 3
+assert end_zeros(101010) == 1
+assert end_zeros(10001001) == 0
+assert end_zeros(100010) == 1
+assert end_zeros(100) == 2
+assert end_zeros(20010) == 1
+assert end_zeros(100001000) == 3
+
+print("The mission is done! ")
+ 
+# expected output :
+#1
+#The mission is done! 
